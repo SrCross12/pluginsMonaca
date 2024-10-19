ec12a4c7dd549a04d66a37684664729bf624ae65
SrCross12 <novaishigor7@gmail.com>
2024-10-19 10:53:48 +0900

    s

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,11 +1,7 @@
-// This is a JavaScript file
-
 $(document).on("click", "#alerta", function(){
-
     function retorno(){
     }
-    navigator.notification.alert("mensagem legal", retorno, "Aviso!", "aceito");
-    
+    navigator.notification.alert("mensagem legal", retorno, "Aviso!","aceito");
 });
 
 $(document).on("click", "#confirm", function(){
@@ -17,15 +13,14 @@ $(document).on("click", "#confirm", function(){
             navigator.notification.alert("Escolheu a opção B");
         }
     }
-        navigator.notification.confirm("Escolha A ou B", confirma, "Escolha: ", ['A','B']);
-    
+navigator.notification.confirm("Escolha A ou B", confirma, "Escolha:",['B','A']);
 });
 
 $(document).on("click", "#beep", function(){
     navigator.notification.beep(4);
 });
 
-$(document).on("click", "#vibracao", function(){
+$(document).on("click", "#vibrar", function(){
     document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     console.log(navigator.vibrate([8000]));
