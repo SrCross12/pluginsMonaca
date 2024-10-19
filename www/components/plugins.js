fb7721de244459600ad5f296d71a807417c04aff
SrCross12 <novaishigor7@gmail.com>
2024-10-19 10:00:05 +0900

    aaa

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -4,7 +4,7 @@ $(document).on("click", "#alerta", function(){
 
     function retorno(){
     }
-    navigator.notification.alert("mensagem legal", retorno, "aviso!", "aceito");
+    navigator.notification.alert("mensagem legal", retorno, "Aviso!", "aceito");
     
 });
 
@@ -12,14 +12,22 @@ $(document).on("click", "#confirm", function(){
     function confirma(buttonIndex){
 
         if(buttonIndex == "1") {
-            navigator.notification.alert("Escolheu a opção A")
+            navigator.notification.alert("Escolheu a opção A");
         } else {
-            navigator.notification.alert("Escolheu a opção B")
+            navigator.notification.alert("Escolheu a opção B");
         }
-        navigator.notification.confirm("Escolha A ou B", confirma, "Escolha: ", ['A','B'])
     }
+        navigator.notification.confirm("Escolha A ou B", confirma, "Escolha: ", ['A','B']);
+    
 });
 
 $(document).on("click", "#beep", function(){
     navigator.notification.beep(4);
+});
+
+$(document).on("click", "#vibracao", function(){
+    document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.vibrate([8000]));
+}
 });
